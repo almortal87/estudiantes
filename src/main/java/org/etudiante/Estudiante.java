8da0f846102c8076e8a65838b8054b71ebd3b0ad
Alan Arandia <almortal87bmx@gmail.com>
2024-06-10 12:41:28 -0400

    1 and second challenge

---
+++ src/main/java/org/etudiante/Estudiante.java
@@ -0,0 +1,13 @@
+package org.etudiante;
+
+public class Estudiante {
+
+    private int StudentID;
+    private String FirstName;
+    private String LastName;
+    private int Age;
+    private String Grade ;
+    private String FavoriteSubject;
+
+
+}
